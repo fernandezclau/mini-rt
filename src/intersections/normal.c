8001f454353b3e8c7f325607d7088169aff3894f
kde-la-c <kdelacolinaplant@gmail.com>
2025-01-04 22:24:32 +0100

    cone normmal done? wip in initial cone hits

--- src/intersections/normal.c
+++ src/intersections/normal.c
@@ -73,53 +73,23 @@ void	set_cylinder_normal(t_cylinder *cy, t_hit *l_hit)
  * @param l_hit The hit structure storing the intersection details,
  * including the position of the hit and the normal to be calculated.
  */
-void	set_cone_normal(t_cone *cn, t_hit *l_hit) //TODO wip on normal formula, don't forget base normal
+void	set_cone_normal(t_cone *cn, t_hit *l_hit)
 {
-	(void)cn;(void)l_hit;
-	// t_vector3	bp;
-	// float		distance;
-	// t_vector3	bp_paral;
-	// t_vector3	bp_perp;
-	// float		m;
-	// t_vector3	bp_perp_scaled;
+	t_vector3	bp;
+	t_vector3	ap;
+	t_vector3	ab;
+	t_vector3	ap_proj;
+	t_vector3	ab_proj;
 
-	// bp = substract_v3(l_hit->position, cn->center);
-	// distance = dot_product_v3(bp, cn->direction);
-	// bp_paral = sum_v3(cn->center, scale_v3(cn->direction, distance));
-	// bp_perp = substract_v3(bp, bp_paral);
-	// m = (cn->diameter / 2) / cn->height;
-	// bp_perp_scaled = scale_v3(bp_perp, m);
-	// if (distance <= EPSILON_4)
-	// 	l_hit->normal = scale_v3(cn->direction, -1);
-	// else
-	// {
-	// 	l_hit->normal = normalize_v3(sum_v3(bp_paral, bp_perp_scaled));
-	// 	// usleep(100000);
-	// 	display_v3(l_hit->normal);
-	// }
-
-	t_vector3	bp = substract_v3(l_hit->position, cn->center);
+	bp = substract_v3(l_hit->position, cn->center);
 	if (dot_product_v3(bp, cn->direction) <= EPSILON_4)
 		l_hit->normal = scale_v3(cn->direction, -1);
 	else
 	{
-		t_vector3	ap = substract_v3(l_hit->position, cn->apex);
-		t_vector3	ab = substract_v3(cn->center, cn->apex);
-		t_vector3	ap_proj = scale_v3(ab, dot_product_v3(ap, ab));
-		t_vector3	bc = scale_v3(normalize_v3(sum_v3(ap_proj, ap)), cn->diameter / 2);
-		t_vector3	ac = sum_v3(ab, bc);
-		l_hit->normal = normalize_v3(sum_v3(ab, scale_v3(ac, dot_product_v3(ab, ac))));
+		ap = normalize_v3(substract_v3(l_hit->position, cn->apex));
+		ab = normalize_v3(substract_v3(cn->center, cn->apex));
+		ap_proj = scale_v3(ab, dot_product_v3(ap, ab));
+		ab_proj = scale_v3(ap, dot_product_v3(ap_proj, ap));
+		l_hit->normal = scale_v3(sum_v3(ap_proj, ab_proj), -1);
 	}
-/* 
-		A
-		|\
-		| \
-		|  \
-		|   \
-   ap_p +    + P
-		|     \
-		|_     \
-		|_|_____\
-		B	     C
- */
 }
